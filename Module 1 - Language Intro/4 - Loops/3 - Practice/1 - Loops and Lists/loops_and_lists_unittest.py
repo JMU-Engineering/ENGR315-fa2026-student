69dada04b3d5fb6660a0fc08cbf2cd69262e5ded
Jason Forsyth <forsy2jb@jmu.edu>
2026-08-20 14:49:14 -0400

    Module 1

---
+++ Module 1 - Language Intro/4 - Loops/3 - Practice/1 - Loops and Lists/loops_and_lists_unittest.py
@@ -0,0 +1,14 @@
+import unittest
+import loops_and_lists as pp
+
+class TestWhileLoop(unittest.TestCase):
+    def test_increment(self):
+        self.assertNotEqual(pp.increment, 0, "Your incrementer does not increase")
+        self.assertEqual(pp.increment, 4)
+
+    def test_total(self):
+        self.assertNotEqual(pp.total, 0, "Your total doesn't add up")
+        self.assertEqual(pp.total, 42)
+
+if __name__ == '__main__':
+    unittest.main()
